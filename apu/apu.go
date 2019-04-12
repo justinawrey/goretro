3e59d5a706c6c259b10c66c53c0375d66488683a
Justin Awrey <awreyjustin@gmail.com>
2019-04-11 21:26:33 -0700

    add comments to apu package

--- apu/apu.go
+++ apu/apu.go
@@ -1,13 +1,23 @@
+// Package apu provides functionality related to the nes audio processing unit.
 package apu
 
+// APU is the audio processing unit of the nes.
 type APU struct {
 }
 
+// New creates a new APU.
 func New() (a *APU) {
 	return &APU{}
 }
 
+// ReadRegister implements mmio.MemoryMappedIO.
 func (a *APU) ReadRegister(address uint16) (data byte) { return 0x00 }
+
+// WriteRegister implements mmio.MemoryMappedIO.
 func (a *APU) WriteRegister(address uint16, data byte) {}
-func (a *APU) Init()                                   {}
-func (a *APU) Clear()                                  {}
+
+// Init implements nes.Module.
+func (a *APU) Init() {}
+
+// Clear implements nes.Module.
+func (a *APU) Clear() {}
