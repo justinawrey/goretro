1a84fc676789414eb0b603b185f4d7b277b7a359
Justin Awrey <awreyjustin@gmail.com>
2019-04-11 21:33:06 -0700

    add comments for display

--- display/display.go
+++ display/display.go
@@ -1,7 +1,15 @@
+// Package display provides functionality related to the UI of the emulator.
 package display
 
+// Display holds all information regarding the UI of the emulator.
+// It is not emulation specific.
 type Display struct{}
 
-func New() (d *Display)   { return &Display{} }
-func (d *Display) Init()  {}
+// New creates a new Display.
+func New() (d *Display) { return &Display{} }
+
+// Init implements nes.Module.
+func (d *Display) Init() {}
+
+// Clear implements nes.Module.
 func (d *Display) Clear() {}
