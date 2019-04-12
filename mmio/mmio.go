1f5c8a0fe357ab4778e098cb94c03b754aab9abe
Justin Awrey <awreyjustin@gmail.com>
2019-04-12 08:58:13 -0700

    comment mmio

--- mmio/mmio.go
+++ mmio/mmio.go
@@ -1,10 +1,13 @@
 package mmio
 
 // MemoryMappedIO specifies a module which can be read from / written to via memory mapped io.
-// Rather than directly exposing the entire ppu / apu / joystick objects to memory,
-// we instead only expose their read / write methods.
-// See Memory for main usage.
+// Each MemoryMappedIO module exposes ReadRegister and WriteRegister functions which perform
+// differently for each module.  For example, the nes PPU may enable writing to certain control registers
+// and input devices may enable reading from certain input feedback registers.
 type MemoryMappedIO interface {
+	// ReadRegister reads a byte of data from a certain special purpose 16-bit register
 	ReadRegister(uint16) byte
+
+	// WriteRegister writes a byte of data to a certain special purpose 16-bit register
 	WriteRegister(uint16, byte)
 }
