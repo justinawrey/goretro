e311f69d5742983da860e53dabd6e780d1c433af
Justin Awrey <awreyjustin@gmail.com>
2024-09-04 10:27:38 -0700

    implement js side key controls

--- internal/standalone/frontend/src/input.ts
+++ internal/standalone/frontend/src/input.ts
@@ -18,11 +18,49 @@ const REQUEST_INPUT_RIGHT = 'request-input-right'
 const REQUEST_INPUT_DOWN = 'request-input-down'
 const REQUEST_INPUT_LEFT = 'request-input-left'
 
-EventsOn(REQUEST_INPUT_A, () => EventsEmit(RECEIVE_INPUT_A, 'a'))
-EventsOn(REQUEST_INPUT_B, () => EventsEmit(RECEIVE_INPUT_B, 'b'))
-EventsOn(REQUEST_INPUT_SELECT, () => EventsEmit(RECEIVE_INPUT_SELECT, 'select'))
-EventsOn(REQUEST_INPUT_START, () => EventsEmit(RECEIVE_INPUT_START, 'start'))
-EventsOn(REQUEST_INPUT_UP, () => EventsEmit(RECEIVE_INPUT_UP, 'up'))
-EventsOn(REQUEST_INPUT_RIGHT, () => EventsEmit(RECEIVE_INPUT_RIGHT, 'right'))
-EventsOn(REQUEST_INPUT_DOWN, () => EventsEmit(RECEIVE_INPUT_DOWN, 'down'))
-EventsOn(REQUEST_INPUT_LEFT, () => EventsEmit(RECEIVE_INPUT_LEFT, 'left'))
+EventsOn(REQUEST_INPUT_A, () => emitKeyState(RECEIVE_INPUT_A, 'a'))
+EventsOn(REQUEST_INPUT_B, () => emitKeyState(RECEIVE_INPUT_B, 'b'))
+EventsOn(REQUEST_INPUT_SELECT, () =>
+    emitKeyState(RECEIVE_INPUT_SELECT, 'select'),
+)
+EventsOn(REQUEST_INPUT_START, () => emitKeyState(RECEIVE_INPUT_START, 'start'))
+EventsOn(REQUEST_INPUT_UP, () => emitKeyState(RECEIVE_INPUT_UP, 'up'))
+EventsOn(REQUEST_INPUT_RIGHT, () => emitKeyState(RECEIVE_INPUT_RIGHT, 'right'))
+EventsOn(REQUEST_INPUT_DOWN, () => emitKeyState(RECEIVE_INPUT_DOWN, 'down'))
+EventsOn(REQUEST_INPUT_LEFT, () => emitKeyState(RECEIVE_INPUT_LEFT, 'left'))
+
+const pressedKeys: Record<string, boolean> = {}
+window.addEventListener('keydown', (e) => (pressedKeys[e.key] = true))
+window.addEventListener('keyup', (e) => (pressedKeys[e.key] = false))
+
+const BUTTON_MAPPINGS = {
+    a: 'j',
+    b: 'k',
+    select: '1',
+    start: '2',
+    up: 'w',
+    right: 'd',
+    down: 's',
+    left: 'a',
+}
+
+function emitKeyState(
+    event:
+        | typeof RECEIVE_INPUT_A
+        | typeof RECEIVE_INPUT_B
+        | typeof RECEIVE_INPUT_SELECT
+        | typeof RECEIVE_INPUT_START
+        | typeof RECEIVE_INPUT_UP
+        | typeof RECEIVE_INPUT_RIGHT
+        | typeof RECEIVE_INPUT_DOWN
+        | typeof RECEIVE_INPUT_LEFT,
+    button: keyof typeof BUTTON_MAPPINGS,
+) {
+    const key = BUTTON_MAPPINGS[button]
+    if (!(key in pressedKeys)) {
+        EventsEmit(event, false)
+        return
+    }
+
+    EventsEmit(event, pressedKeys[key])
+}
